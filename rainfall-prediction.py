b16b15660f486f1260177c167047a2a2ef8690dc
dnath <nathdip@gmail.com>
2017-12-07 17:28:39 +0100

    Minor Changes

--- rainfall-prediction.py
+++ rainfall-prediction.py
@@ -15,7 +15,17 @@ import seaborn as sns
 current_directory = os.getcwd()
 dir_list = next(os.walk('.'))[1]
 
-os.chdir(dir_list[0])
+os.chdir(dir_list[1])
 files_xls  = glob.glob('*.xls')
 data_pcp = pd.read_excel(files_xls[0])
 pcp_filenames = data_pcp['NAME']
+
+pcp_dict = {}
+#for i in range(0, len(pcp_filenames)):
+data = open(pcp_filenames[2]+'.txt')
+precipitation = np.loadtxt(data, delimiter = ',')
+date = str(int(precipitation[0]))
+precipitation = precipitation[1:-1]
+pcp_dict[date] = precipitation
+print(pcp_dict)
+    
