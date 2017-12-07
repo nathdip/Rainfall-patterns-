74357a563919fac413de4935be7d0da01d6b01da
bhumi25 <bhumika25uniyal@gmail.com>
2017-12-07 21:42:11 +0100

    First commit by Bhumi

--- rainfall-prediction.py
+++ rainfall-prediction.py
@@ -15,17 +15,18 @@ import seaborn as sns
 current_directory = os.getcwd()
 dir_list = next(os.walk('.'))[1]
 
-os.chdir(dir_list[1])
+os.chdir(dir_list[3])
 files_xls  = glob.glob('*.xls')
 data_pcp = pd.read_excel(files_xls[0])
 pcp_filenames = data_pcp['NAME']
 
 pcp_dict = {}
-#for i in range(0, len(pcp_filenames)):
-data = open(pcp_filenames[2]+'.txt')
-precipitation = np.loadtxt(data, delimiter = ',')
-date = str(int(precipitation[0]))
-precipitation = precipitation[1:-1]
-pcp_dict[date] = precipitation
+for i in range(0, len(pcp_filenames)):
+    data = open(pcp_filenames[i]+'.txt')
+    precipitation = np.loadtxt(data, delimiter = ',')
+    precipitation = precipitation[1:-1]
+    missing_data_counts = sum(precipitation == -999.0)
+    pcp_dict[pcp_filenames[i]] = missing_data_counts
+    print(pcp_filenames[i], missing_data_counts)
 print(pcp_dict)
     
